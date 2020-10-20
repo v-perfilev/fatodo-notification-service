1097d203a794fe68e40d734bff176a661d58be1d
Vladimir Perfilev <persoff68@gmail.com>
2020-10-20 23:46:13 +0200

    setup databases

--- src/main/java/com/persoff68/fatodo/config/DatabaseConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/DatabaseConfiguration.java
@@ -1,72 +1,31 @@
 package com.persoff68.fatodo.config;
 
-import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongo3Driver;
-import com.github.cloudyrock.spring.v5.MongockSpring5;
 import com.persoff68.fatodo.config.constant.AppConstants;
+import liquibase.integration.spring.SpringLiquibase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.mongodb.core.index.IndexOperations;
-import org.springframework.data.mongodb.core.index.IndexResolver;
-import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
-import org.springframework.data.mongodb.core.mapping.BasicMongoPersistentEntity;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
-@EnableMongoRepositories(basePackages = AppConstants.REPOSITORY_PATH)
-@EnableMongoAuditing(auditorAwareRef = "securityAuditorAware")
+@EnableJpaRepositories(basePackages = AppConstants.REPOSITORY_PATH)
+@EnableJpaAuditing(auditorAwareRef = "securityAuditorAware")
+@EnableTransactionManagement
 @RequiredArgsConstructor
 public class DatabaseConfiguration {
 
-    private final MongoTemplate mongoTemplate;
-    private final MongoConverter mongoConverter;
+    private final DataSource dataSource;
 
     @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener() {
-        return new ValidatingMongoEventListener(validator());
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-        return new LocalValidatorFactoryBean();
-    }
-
-    @Bean
-    public MongockSpring5.MongockInitializingBeanRunner mongockInitializingBeanRunner(
-            ApplicationContext springContext,
-            MongoTemplate mongoTemplate) {
-        String scanPath = this.getClass().getPackageName() + ".database.migrations";
-        return MongockSpring5.builder()
-                .setDriver(SpringDataMongo3Driver.withDefaultLock(mongoTemplate))
-                .addChangeLogsScanPackage(scanPath)
-                .setSpringContext(springContext)
-                .buildInitializingBeanRunner();
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void initIndicesAfterStartup() {
-        Object mappingContext = this.mongoConverter.getMappingContext();
-        if (mappingContext instanceof MongoMappingContext) {
-            MongoMappingContext mongoMappingContext = (MongoMappingContext) mappingContext;
-            for (BasicMongoPersistentEntity<?> persistentEntity : mongoMappingContext.getPersistentEntities()) {
-                Class<?> clazz = persistentEntity.getType();
-                if (clazz.isAnnotationPresent(Document.class)) {
-                    IndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
-                    IndexOperations indexOps = mongoTemplate.indexOps(clazz);
-                    resolver.resolveIndexFor(clazz).forEach(indexOps::ensureIndex);
-                }
-            }
-        }
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/master.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
     }
 
 }
