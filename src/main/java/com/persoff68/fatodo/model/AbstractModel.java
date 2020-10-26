42764b98d32ce0bbdd602ec88164e41c7aad9037
Vladimir Perfilev <persoff68@gmail.com>
2020-10-26 22:37:21 +0100

    TDM-72 edit auditable

--- src/main/java/com/persoff68/fatodo/model/AbstractModel.java
+++ src/main/java/com/persoff68/fatodo/model/AbstractModel.java
@@ -1,14 +1,21 @@
 package com.persoff68.fatodo.model;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.util.UUID;
 
 @Data
+@MappedSuperclass
 public abstract class AbstractModel {
 
     @Id
+    @GeneratedValue(generator = "UUID", strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     protected UUID id;
 
 }
